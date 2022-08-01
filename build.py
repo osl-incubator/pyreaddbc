11115625913e3e4d3a2bf5a220312da97dbe7414
esloch <es.loch@gmail.com>
2022-08-01 13:38:43 -0300

    feat: Add initial package structure

---
+++ build.py
@@ -0,0 +1,53 @@
+from distutils.command.build_ext import build_ext
+from distutils.core import Extension
+from distutils.errors import (  # noqa E501
+    CCompilerError,
+    DistutilsExecError,
+    DistutilsPlatformError,
+)
+from pathlib import Path
+
+BASE_DIR = Path().resolve()
+
+ext_modules = [
+    Extension(
+        "_readdbc",
+        sources=[
+            "pyreaddbc/c-src/dbc2dbf.c",
+            "pyreaddbc/c-src/blast.c",
+        ],
+        include_dirs=["pyreaddbc/c-src/"],
+    ),
+]
+
+
+class BuildFailed(Exception):
+    pass
+
+
+class ExtBuilder(build_ext):
+    def run(self):
+        try:
+            build_ext.run(self)
+        except (DistutilsPlatformError, FileNotFoundError):
+            raise BuildFailed("File not found. Could not compile C extension.")
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except (
+            CCompilerError,
+            DistutilsExecError,
+            DistutilsPlatformError,
+            ValueError,
+        ):
+            raise BuildFailed("Could not compile C extension.")
+
+
+def build(setup_kwargs):
+    """
+    This function is mandatory in order to build the extensions.
+    """
+    setup_kwargs.update(
+        {"ext_modules": ext_modules, "cmdclass": {"build_ext": ExtBuilder}}
+    )
---
+++ pyreaddbc/_build_readdbc.py
@@ -0,0 +1,37 @@
+u"""
+Created on 16/08/16
+by fccoelho
+license: GPL V3 or Later
+"""
+import os
+
+from cffi import FFI
+
+ffibuilder = FFI()
+
+pyreaddbc_PATH = os.path.dirname(__file__)
+ROOT_PATH = os.path.dirname(pyreaddbc_PATH)
+
+with open(os.path.join(pyreaddbc_PATH, "c-src/dbc2dbf.c"), "r") as f:
+    ffibuilder.set_source(
+        "_readdbc",
+        f.read(),
+        libraries=["c"],
+        sources=[os.path.join(pyreaddbc_PATH, "c-src/blast.c")],
+        include_dirs=[os.path.join(pyreaddbc_PATH, "c-src/")],
+    )
+ffibuilder.cdef(
+    """
+    static unsigned inf(void *how, unsigned char **buf);
+    static int outf(void *how, unsigned char *buf, unsigned len);
+    void dbc2dbf(char** input_file, char** output_file);
+    """
+)
+
+with open(os.path.join(pyreaddbc_PATH, "c-src/blast.h")) as f:
+    ffibuilder.cdef(f.read(), override=True)
+
+if __name__ == "__main__":
+    ffibuilder.compile(
+        tmpdir="/tmp/pyreaddbc", target=f"{ROOT_PATH}/bin/_readdbc.so", verbose=True, debug=True
+    )
