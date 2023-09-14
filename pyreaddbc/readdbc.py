da34ecc43e09c3538b405e11151bcdcfb0caa9d6
Sandro Loch <es.loch@gmail.com>
2023-09-14 14:13:04 -0300

    feat(module): Remove unnecessary functions from the readdbc module

--- pyreaddbc/readdbc.py
+++ pyreaddbc/readdbc.py
@@ -4,40 +4,11 @@ by fccoelho
 license: GPL V3 or Later
 """
 import os
-from tempfile import NamedTemporaryFile
-
-import pandas as pd
-from dbfread import DBF
 
 try:
     from pyreaddbc._readdbc import ffi, lib
 except (ImportError, ModuleNotFoundError):
     from ._readdbc import ffi, lib
-
-
-def read_dbc(filename, encoding="utf-8", raw=False):
-    """
-    Opens a DBC file and return its contents as a pandas
-    Dataframe.
-    :param filename: .dbc filename
-    :param encoding: encoding of the data
-    :param raw: |
-        Skip type conversion. Set it to True to avoid type conversion errors
-    :return: Pandas Dataframe.
-    """
-    if isinstance(filename, str):
-        filename = filename.encode()
-    with NamedTemporaryFile(delete=False) as tf:
-        dbc2dbf(filename, tf.name.encode())
-        try:
-            dbf = DBF(tf.name, encoding=encoding, raw=raw)
-            df = pd.DataFrame(list(dbf))
-        except ValueError:
-            dbf = DBF(tf.name, encoding=encoding, raw=not raw)
-            df = pd.DataFrame(list(dbf))
-    os.unlink(tf.name)
-
-    return df
 
 
 def dbc2dbf(infile, outfile):
@@ -54,13 +25,3 @@ def dbc2dbf(infile, outfile):
     q = ffi.new("char[]", os.path.abspath(outfile))
 
     lib.dbc2dbf([p], [q])
-
-
-def read_dbc_dbf(filename: str):
-    if filename.endswith(("dbc", "DBC")):
-        df = read_dbc(filename, encoding="iso-8859-1")
-    elif filename.endswith(("DBF", "dbf")):
-        dbf = DBF(filename, encoding="iso-8859-1")
-        df = pd.DataFrame(list(dbf))
-
-    return df
